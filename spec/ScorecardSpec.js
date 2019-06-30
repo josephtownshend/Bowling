f4a6a3b0d019333d176daacddcf92777576068d2
joseph townshend <joetownshend@mac.com>
2019-06-30 15:19:58 +0100

    failing test written for default scorecard score of 0

--- spec/ScorecardSpec.js
+++ spec/ScorecardSpec.js
@@ -2,4 +2,25 @@
 
 describe('Scorecard', function() {
 
+  var scorecard;
+
+  beforeEach(function() {
+    scorecard = new Scorecard;
+  });
+
+  describe('Default', function() {
+    it('Has a score of 0', function() {
+      expect(scorecard.score()).toEqual(0)
+    });
+
+
+  });
+
+
+
+
+
+
+
+
 })
